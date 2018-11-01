86a7624fa6bdfe672df8286dc06f758534195609
Shriggs <spm1215@gmail.com>
2018-10-31 21:22:14 -0500

    Yeeet

---
+++ TODO-Vue/src/components/Landing.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <img src="https://sandboxcodes.com/images/purplelogo.svg">
+        <h1>Welcome to Todue, the best way to TODO</h1>
+        <router-link to="/login">Get started</router-link>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+</style>
