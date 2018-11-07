29e8b11b6cfee9235068496a522fee5862878b42
Shriggs <spm1215@gmail.com>
2018-11-06 18:38:20 -0600

    Finish Pokedex bare

--- Pokedex/script.js
+++ Pokedex/script.js
@@ -11,35 +11,77 @@ $(function() {
         }
     });
 
-    var fetchName = function (num) {
+    var toTitleCase = function (txt) {
+        return txt.toLowerCase()
+            .split(' ')
+            .map((s) => s.charAt(0).toUpperCase() + s.substring(1))
+            .join(' ');
+    }
+
+    var getName = function (data) {
         let name = "";
+
+        name = toTitleCase(data.name);
+
+        return name;
     };
 
-    var fetchImage = function (num) {
+    var getImage = function (data) {
         let image = "";
+
+        image = data.sprites.front_default;
+
+        return image;
     };
 
-    var fetchTypes = function (num) {
+    var getTypes = function (data) {
         let types = [];
+
+        data.types.forEach(type => {
+            types.push(toTitleCase(type.type.name));
+        });
+
+        return types;
     };
 
-    var fetchStats = function (num) {
+    var getStats = function (data) {
         let stats = {};
+
+        data.stats.forEach(stat => {
+            stats[toTitleCase(stat.stat.name.replace("-", " "))] = stat.base_stat;
+        });
+
+        return stats;
     };
 
-    var fetchAbilities = function (num) {
+    var getAbilities = function (data) {
         let abilities = [];
+
+        data.abilities.forEach(ability => {
+            abilities.push(toTitleCase(ability.ability.name));
+        });
+
+        return abilities;
     };
 
-    var initPokemon = function (num) {
-        // app.pokemon =
+    var fetchPokemon = function (num, callback) {
         axios.get("https://pokeapi.co/api/v2/pokemon/" + num + "/")
             .then(function (res) {
-                console.log(res.data);
+                callback(res.data);
             })
             .catch(function (err) {
-                console.log(err);
+                app.valid = false;
             })
+    }
+
+    var initPokemon = function (num) {
+        fetchPokemon(num, function (data) {
+            app.pokemon = getName(data);
+            app.image = getImage(data);
+            app.types = getTypes(data);
+            app.stats = getStats(data);
+            app.abilities = getAbilities(data);
+        });
     };
 
     $('#pokeInput').on('input', function (e) {
