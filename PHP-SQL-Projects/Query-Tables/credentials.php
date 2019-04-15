f85caf23f35d339adfa1a5fc15a791538bfa52b3
Shriggs <spm1215@gmail.com>
2019-04-15 10:33:57 -0500

    dfxvc

--- PHP-SQL-Projects/Query-Tables/credentials.php
+++ PHP-SQL-Projects/Query-Tables/credentials.php
@@ -1,11 +1,11 @@
 <?php
 
-//$servername = "localhost";
-//$username = "root";
-//$password = "usbw";
-//$dbname = "sakila";
+$servername = "localhost";
+$username = "root";
+$password = "usbw";
+$dbname = "sakila";
 
- $servername = "localhost:3306";
- $username = "root";
- $password = "radvokMySQL16";
- $dbname = "sakila";
+ // $servername = "localhost:3306";
+ // $username = "root";
+ // $password = "radvokMySQL16";
+ // $dbname = "sakila";
--- PHP-SQL-Projects/Query-Tables/films.php
+++ PHP-SQL-Projects/Query-Tables/films.php
@@ -3,33 +3,14 @@
 include 'credentials.php';
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $db);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn -> connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 
-//$query = "SELECT title, description, release_year FROM film";
-//
-//$result = $conn->query($query);
-//
-//echo "<table>";
-//echo "  <tr>";
-//echo "      <th>Name</th>";
-//echo "      <th>Description</th>";
-//echo "      <th>Release Year</th>";
-//echo "  </tr>";
-//while ($row = $result->fetch_assoc()) {
-//    echo "  <tr>";
-//    echo "      <td>" . $row['title'] . "</td>";
-//    echo "      <td>" . $row['description'] . "</td>";
-//    echo "      <td>" . $row['release_year'] . "</td>";
-//    echo "  </tr>";
-//}
-//echo "</table>";
-
-$query = "SELECT * FROM film";
+$query = "SELECT title, description, release_year FROM film";
 
 $result = $conn->query($query);
 
@@ -40,12 +21,31 @@ echo "      <th>Description</th>";
 echo "      <th>Release Year</th>";
 echo "  </tr>";
 while ($row = $result->fetch_assoc()) {
-    echo "  <tr>";
-//    echo "      <td>" . $row['title'] . "</td>";
-//    echo "      <td>" . $row['description'] . "</td>";
-//    echo "      <td>" . $row['release_year'] . "</td>";
-    echo "  </tr>";
+   echo "  <tr>";
+   echo "      <td>" . $row['title'] . "</td>";
+   echo "      <td>" . $row['description'] . "</td>";
+   echo "      <td>" . $row['release_year'] . "</td>";
+   echo "  </tr>";
 }
 echo "</table>";
+
+// $query = "SELECT * FROM film";
+//
+// $result = $conn->query($query);
+//
+// echo "<table>";
+// echo "  <tr>";
+// echo "      <th>Name</th>";
+// echo "      <th>Description</th>";
+// echo "      <th>Release Year</th>";
+// echo "  </tr>";
+// while ($row = $result->fetch_assoc()) {
+//     echo "  <tr>";
+// //    echo "      <td>" . $row['title'] . "</td>";
+// //    echo "      <td>" . $row['description'] . "</td>";
+// //    echo "      <td>" . $row['release_year'] . "</td>";
+//     echo "  </tr>";
+// }
+// echo "</table>";
 
 ?>
